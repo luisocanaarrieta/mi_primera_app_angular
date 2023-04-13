903d4d13e1f29bc184dd416946b7698aba7df65c
luis ocana <luis.ocana.arrieta@gmail.com>
2023-04-13 00:12:21 -0500

    Se configuro bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'la primera app que estoy haciendo';
-  secondaryTitle = 'Este es emi titulo oficial'
+  titulo = 'Hola bienvenidos a esta aplicacion en Angular';
 }
